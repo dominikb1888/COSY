e459925b465f77bb86d213ef4d575ba73e4a80b8
Dominik Boehler <dominik.boehler@gmx.net>
2023-11-30 08:59:53 +0100

    updates

--- repos_to_md.sh
+++ repos_to_md.sh
@@ -5,7 +5,7 @@ SEPARATOR="|---|---|---|"
 
 echo $HEADER > bloglist_auto.md &&\
 echo $SEPARATOR >> bloglist_auto.md &&\
-gh repo list 23W-GBAC -L 100 --json name --json description --json stargazerCount --json homepageUrl\
+gh repo list 23W-gph -L 100 --json name --json description --json stargazerCount --json homepageUrl\
 | jq -r '.[] | [.name, .homepageUrl, .description] | @tsv' | sed 's/\t/ | /g' \
 | sed 's/^/| /g'\
 | sed 's/$/ |/g' >> bloglist_auto.md
